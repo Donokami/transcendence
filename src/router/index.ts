d901aeae8fa8586b0cf3e80a604fdc6e973f67cf
Lucas Lethuillier <lucas.lethuillier@gmail.com>
2023-04-19 16:06:21 +0200

    Game page updated

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AuthView from '../views/AuthView.vue'
 import ChatView from '../views/ChatView.vue'
+import GameView from '../views/GameView.vue'
 import HomeView from '../views/HomeView.vue'
 import StatsView from '../views/StatsView.vue'
 import ProfileView from '../views/ProfileView.vue'
@@ -20,6 +21,11 @@ const routes = [
     component: ChatView,
     path: '/chat',
     name: 'chat'
+  },
+  {
+    component: GameView,
+    path: '/game',
+    name: 'game'
   },
   {
     component: StatsView,
---
+++ src/views/GameView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="max-w-screen-xl min-w-[95%] lg:mx-auto text-black">
+    <site-header></site-header>
+    <div class="border-black border-2 flex flex-col mx-2 my-3 mt-1 p-5 items-center">
+      <h2 class="text-2xl font-bold mb-4 text-black">[Room X]</h2>
+      <p class="text-2xl font-bold mb-4 text-black">Conobi vs Hayce_</p>
+      <p class="text-2xl font-bold mb-4 text-black">5 - 4</p>
+      <div class="w-3/4 h-96 flex items-center relative">
+        <game-canvas></game-canvas>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import SiteHeader from '../components/SiteHeader.vue'
+import GameCanvas from '../components/GameCanvas.vue'
+
+export default {
+  name: 'ProfileView',
+  components: {
+    SiteHeader,
+    GameCanvas
+  },
+  data() {
+    return {}
+  },
+  mounted() {},
+  methods: {}
+}
+</script>
