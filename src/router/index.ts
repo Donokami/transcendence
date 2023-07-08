0325e25a03348da6d42fa845fc8f94ea7ddef62b
Haycee <95182577+Haycee@users.noreply.github.com>
2023-07-08 17:43:37 +0200

    Fix 2FA in OAuth and resolve Vue warnings

--- src/router/index.ts
+++ src/router/index.ts
@@ -49,7 +49,7 @@ const routes: Array<RouteRecordRaw> = [
   {
     component: NotFoundView,
     path: '/:pathMatch(.*)*',
-    name: 'NotFound',
+    name: 'NotFound'
   }
 ]
 
@@ -60,15 +60,14 @@ const router = createRouter({
 })
 
 router.beforeEach(async (to, from, next) => {
-  const userStore = useUserStore();
+  const userStore = useUserStore()
   if (!userStore.loggedUser) {
-    await userStore.refreshUser();
+    await userStore.refreshUser()
   }
-  if (to.name !== "auth" && !userStore.loggedUser) {
-      next({ name: "auth" });
-  } 
-  else {
-    next();
+  if (to.name !== 'auth' && to.name !== 'mfa' && !userStore.loggedUser) {
+    next({ name: 'auth' })
+  } else {
+    next()
   }
 })
 
